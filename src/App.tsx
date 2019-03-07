30d14a4d8fca625c1745631284b1002f69c18fb4
mikeonslow <mikeonslow@gmail.com>
2019-03-06 23:40:12 -0500

    WIP - Currently does not compile

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,83 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Grid from '@material-ui/core/Grid';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import Paper from '@material-ui/core/Paper';
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+class GuttersGrid extends React.Component<{name: string}, { children?: any; }> {
+  state: any = {
+    spacing: '16',
+  };
 
-function ButtonAppBar(props: any) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+  handleChange = (key: any) => (event: any, value: any) => {
+    this.setState({
+      [key]: value,
+    });
+  };
+
+  render() {
+
+    console.log(this.props);
+
+    // const { classes } = this.props;
+    const { spacing } = this.state;
+
+    return (
+      <Grid container className="" spacing={16}>
+        <Grid item xs={12}>
+          <Grid container className="" justify="center">
+            {[0, 1, 2].map(value => (
+              <Grid key={value} item>
+                <Paper className="" />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className="">
+            <Grid container>
+              <Grid item>
+                <FormLabel>spacing</FormLabel>
+                <RadioGroup
+                  name="spacing"
+                  aria-label="Spacing"
+                  value={spacing}
+                  onChange={this.handleChange('spacing')}
+                  row
+                >
+                  <FormControlLabel value="0" control={<Radio />} label="0" />
+                  <FormControlLabel value="8" control={<Radio />} label="8" />
+                  <FormControlLabel value="16" control={<Radio />} label="16" />
+                  <FormControlLabel value="24" control={<Radio />} label="24" />
+                  <FormControlLabel value="32" control={<Radio />} label="32" />
+                  <FormControlLabel value="40" control={<Radio />} label="40" />
+                </RadioGroup>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
-ButtonAppBar.propTypes = {
+GuttersGrid.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(ButtonAppBar)
+export default withStyles({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+})(GuttersGrid);
